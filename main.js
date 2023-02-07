650a0192780fe08f3a3235ae7c4dd0effb709eaf
Liam <phuc01112002@gmail.com>
2023-02-07 17:13:51 +0700

    clean code, done project 2_7_2023

--- main.js
+++ main.js
@@ -1,48 +1,65 @@
 'use strict';
 
-const button = document.querySelector('.btn.check');
-const buttonAgain = document.querySelector('.btn.again');
+const $ = document.querySelector.bind(document);
 
-const randomNumber = Math.floor(Math.random() * 100 + 1);
-
-// console.log(randomNumber);
-let yourTurn = document.querySelector('.label-turn span');
+const bodyElement = $('body');
+const button = $('.btn.check');
+const buttonAgain = $('.btn.again');
+let randomNumber = Math.floor(Math.random() * 100 + 1);
+let yourTurn = $('.label-turn span');
+let yourScore = $('.label-score span');
 let turn = 7;
+let score = 10;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  $('.message').textContent = message;
+};
 
 buttonAgain.addEventListener('click', () => {
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.number').textContent = `?`;
-  document.querySelector('.guess').value = '';
+  displayMessage('Start guessing...');
+  $('.number').textContent = `?`;
+  $('.guess').value = '';
   turn = 7;
+  score = 10;
   yourTurn.textContent = '7';
+  yourScore.textContent = '10';
   button.style.display = 'block';
+  bodyElement.style.backgroundColor = '#222';
+  $('.number').style.width = '15rem';
+  randomNumber = Math.floor(Math.random() * 100 + 1);
+  console.log(randomNumber);
 });
 
 button.addEventListener('click', () => {
-  const result = document.querySelector('.guess').value;
+  const result = $('.guess').value;
 
   if (Number(result) < 1 || Number(result) > 100) {
-    document.querySelector('.message').textContent =
-      'Làm ơn nhập số lớn hơn 1 và bé hơn 0';
+    displayMessage('Làm ơn nhập số lớn hơn 1 và bé hơn 0');
   } else if (Number(result) === randomNumber) {
-    document.querySelector('.message').textContent = 'Congratulations🎉🎉🎉';
-    document.querySelector('.number').textContent = `${randomNumber}`;
+    displayMessage('Congratulations🎉🎉🎉');
+    $('.number').textContent = `${randomNumber}`;
     button.style.display = 'none';
-  } else if (Number(result) < randomNumber) {
-    document.querySelector('.message').textContent = 'Bé hơn mất rồi';
-    turn = turn - 1;
-    yourTurn.innerHTML = `${turn}`;
-    if (turn === 0) {
-      document.querySelector('.message').textContent = 'Đồ thất bại!!! Get out';
-      button.style.display = 'none';
+    bodyElement.style.backgroundColor = '#60b347';
+    $('.number').style.width = '25rem';
+    if (score > highscore) {
+      highscore = score;
+      $('.highscore').textContent = highscore;
     }
   } else {
-    document.querySelector('.message').textContent = 'Lớn hơn mất rồi';
-    turn = turn - 1;
-    yourTurn.innerHTML = `${turn}`;
-    if (turn === 0) {
-      document.querySelector('.message').textContent = 'Đồ thất bại!!! Get out';
-      button.style.display = 'none';
+    if (Number(result) !== randomNumber) {
+      displayMessage(
+        Number(result) > randomNumber ? 'Lớn hơn mất rồi' : 'Bé hơn mất rồi'
+      );
+      turn--;
+      score--;
+      $('.label-score .score').textContent = `${score}`;
+      yourTurn.innerHTML = `${turn}`;
+      if (turn === 0) {
+        displayMessage('Đồ thất bại!!! Get out');
+        button.style.display = 'none';
+        bodyElement.style.backgroundColor = 'red';
+      }
     }
   }
 });
